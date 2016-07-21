034cff6b24a0c2ebdae72375e50b1d436533839e
alphywalfie <azidelapaz@gmail.com>
2016-07-21 19:59:44 +0800

    you can now only change a disappointment if it is your own

--- app/src/main/java/com/example/logan/cameraparsedemo2016/RealmDisappointmentAdapter.java
+++ app/src/main/java/com/example/logan/cameraparsedemo2016/RealmDisappointmentAdapter.java
@@ -2,6 +2,7 @@ package com.example.logan.cameraparsedemo2016;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -24,10 +25,10 @@ import io.realm.RealmBaseAdapter;
  * Created by lance on 21/07/2016.
  */
 public class RealmDisappointmentAdapter extends RealmBaseAdapter<Disappointment> implements ListAdapter {
-    private Activity context;
     private ArrayList<Disappointment> disappointments;
     private File disappointmentImage;
     Realm realm = Realm.getDefaultInstance();
+    SharedPreferences prefs = context.getSharedPreferences("remember_me", context.MODE_PRIVATE);
 
     public RealmDisappointmentAdapter(@NonNull Context context, @Nullable OrderedRealmCollection<Disappointment> data) {
         super(context, data);
@@ -73,8 +74,14 @@ public class RealmDisappointmentAdapter extends RealmBaseAdapter<Disappointment>
             }
             Button db = (Button) v.findViewById(R.id.deleteButton);
             Button eb = (Button) v.findViewById(R.id.editButton);
-            db.setTag(d);
-            eb.setTag(d);
+            String currentUser = prefs.getString("userId", "");
+            if (!d.getUser().equals(currentUser))
+            {
+                db.setVisibility(View.INVISIBLE);
+                eb.setVisibility(View.INVISIBLE);
+                db.setTag(d);
+                eb.setTag(d);
+            }
             return v;
         }
         else
